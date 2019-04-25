866cb60ec2fba7a56d81cb5b218e7b10feb7dedc
lk19940215 <497016113@qq.com>
2019-04-25 12:25:28 +0800

    vue-issue
    a vue.js project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div v-for="i in 2">
+      <div @click="functionClickHandle(i)">{{i}}</div>
+    </div>
+    <div v-for="i in 2">
+      <div @click="itemClick(i)">{{i}}</div>
+    </div>
+  </div>
+  <!-- <p></p> -->
+</template>
+
+<script>
+
+export default {
+  methods: {
+    functionClickHandle(res) {
+      window.alert( res + 'i am big boss')
+      console.log( res + 'i am big boss')
+    },
+    itemClick() {
+      window.alert('item click changed')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app > div {
+  padding: 10px 0;
+}
+</style>
